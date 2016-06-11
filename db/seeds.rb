7eabf4f48cf0253ad3523d297d64bd4eb172b53a
raycewiggins <raycewiggins@gmail.com>
2016-06-10 23:03:47 -0500

    Assignment 30: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,10 @@ require 'random_data'
    )
  end
 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: 'Unique Title', body: 'unique body')
+ puts "#{Post.count}"
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
