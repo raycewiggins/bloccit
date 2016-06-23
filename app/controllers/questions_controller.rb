6db7d07c2cc8b23b9a5afc2deaba1de7a5b2b1da
raycewiggins <raycewiggins@gmail.com>
2016-06-22 23:25:14 -0500

    Assignment 33: Completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -51,7 +51,7 @@ class QuestionsController < ApplicationController
 
        if @question.destroy
          flash[:notice] = "\"#{@question.title}\" was deleted successfully."
-         redirect_to question_path
+         redirect_to questions_path
        else
          flash.now[:alert] = "There was an error deleting the question."
          render :show
