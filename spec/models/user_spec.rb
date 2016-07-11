cfc529f40ba8f8cb0cc25a8d5751740c92340c03
raycewiggins <raycewiggins@gmail.com>
2016-07-10 20:17:39 -0500

    Checkpoint-40: almost completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
 
   it { is_expected.to have_many(:posts) }
-  
+
    # Shoulda tests for name
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -36,7 +36,48 @@ RSpec.describe User, type: :model do
 
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
+      end
     end
 
-  end
+     it "responds to role" do
+       expect(user).to respond_to(:role)
+     end
+
+     it "responds to admin?" do
+       expect(user).to respond_to(:admin?)
+     end
+
+     it "responds to member?" do
+       expect(user).to respond_to(:member?)
+     end
+
+   describe "roles" do
+     it "is member by default" do
+       expect(user.role).to eq("member")
+     end
+
+     context "member user" do
+       it "returns true for #member?" do
+         expect(user.member?).to be_truthy
+       end
+
+       it "returns false for #admin?" do
+         expect(user.admin?).to be_falsey
+       end
+     end
+
+     context "admin user" do
+       before do
+         user.admin!
+       end
+
+       it "returns false for #member?" do
+         expect(user.member?).to be_falsey
+       end
+
+       it "returns true for #admin?" do
+         expect(user.admin?).to be_truthy
+       end
+     end
+   end
 end
