e982625061f7734db3ce0a963e552b5b6d5df7ba
raycewiggins <raycewiggins@gmail.com>
2016-07-13 18:08:17 -0500

    Assignment 43: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
+  after_create :create_vote
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :labelings, as: :labelable
@@ -29,5 +30,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   private
+
+   def create_vote
+     user.votes.create(value: 1, post: self)
    end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -78,4 +78,16 @@ RSpec.describe Post, type: :model do
        end
      end
    end
+
+    describe "#create_vote" do
+      it "sets post up_votes to 1" do
+       expect(post.up_votes).to eq(1)
+      end
+
+      it "calls create_vote when created " do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+    end
 end
