6b47b714d59cb65bc009d4cbfa6109dea4d09552
raycewiggins <raycewiggins@gmail.com>
2016-07-13 15:45:09 -0500

    Checkpoint 43 Voting: Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,21 +5,19 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
-  
-   # Shoulda tests for name
-   it { is_expected.to validate_presence_of(:name) }
-   it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  it { is_expected.to have_many(:votes) }
 
-   # Shoulda tests for email
-   it { is_expected.to validate_presence_of(:email) }
-   it { is_expected.to validate_uniqueness_of(:email) }
-   it { is_expected.to validate_length_of(:email).is_at_least(3) }
-   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-   # Shoulda tests for password
-   it { is_expected.to validate_presence_of(:password) }
-   it { is_expected.to have_secure_password }
-   it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_length_of(:email).is_at_least(3) }
+  it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to have_secure_password }
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
    describe "attributes" do
      it "should have name and email attributes" do
