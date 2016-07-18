74e5a46f5d18555e039211e57be7f9e4b06612fc
raycewiggins <raycewiggins@gmail.com>
2016-07-17 22:39:16 -0500

    Assignment 48 Completed

--- spec/api/v1/controllers/posts_controller_spec.rb
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -1,8 +1,10 @@
-require 'rails_helper'
+ require 'rails_helper'
 
- RSpec.describe Api::V1::TopicsController, type: :controller do
+ RSpec.describe Api::V1::PostsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post) }
+   let(:my_comment) { create(:comment) }
 
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -11,14 +13,14 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_post.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns child posts" do
-       get :show, id: my_topic.id
+     it "GET show returns child comments" do
+       get :show, id: my_post.id
        response_hash = JSON.parse response.body
-       expect(response_hash['posts']).to_not be_nil
+       expect(response_hash['comments']).to_not be_nil
      end
    end
 
@@ -33,14 +35,14 @@ require 'rails_helper'
      end
 
      it "GET show returns http success" do
-       get :show, id: my_topic.id
+       get :show, id: my_post.id
        expect(response).to have_http_status(:success)
      end
 
-     it "GET show returns child posts" do
-       get :show, id: my_topic.id
+     it "GET show returns child comments" do
+       get :show, id: my_post.id
        response_hash = JSON.parse response.body
-       expect(response_hash['posts']).to_not be_nil
+       expect(response_hash['comments']).to_not be_nil
      end
    end
  end
